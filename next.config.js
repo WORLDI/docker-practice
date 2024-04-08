d1f913f65c32586708f9df3c4024708ee1479a69
hejiang <hejiang@taou.com>
2024-04-08 17:13:15 +0800

    feat: add Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,27 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // 注意：不要直接修改 config 对象，而是返回一个新的对象
-        // 例如，添加一个别名（alias）
-        config.resolve.alias['components'] = path.join(__dirname, 'components');
-    
-        // 或者添加一个新的插件
-        // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-    
-        // 你可以根据环境或者其他条件来定制 webpack 配置
-        if (dev) {
-          // 开发模式下的特定配置
-        }
-    
-        if (isServer) {
-          // 服务端打包的特定配置
-        }
-    
-        // 返回修改后的配置对象
-        return config;
-      }
-}
+  output: "standalone",
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // 注意：不要直接修改 config 对象，而是返回一个新的对象
+    // 例如，添加一个别名（alias）
+    config.resolve.alias["components"] = path.join(__dirname, "components");
 
-module.exports = nextConfig
+    // 或者添加一个新的插件
+    // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+
+    // 你可以根据环境或者其他条件来定制 webpack 配置
+    if (dev) {
+      // 开发模式下的特定配置
+    }
+
+    if (isServer) {
+      // 服务端打包的特定配置
+    }
+
+    // 返回修改后的配置对象
+    return config;
+  },
+};
+
+module.exports = nextConfig;
