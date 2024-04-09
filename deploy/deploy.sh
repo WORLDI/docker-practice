1ab6b087a9b7e4f3487b42d52f354fd19499283e
hejiang <hejiang@taou.com>
2024-04-09 11:32:19 +0800

    fix: fix deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,10 @@
 echo "1. stop container"
-sudo docker stop nextjs-docker
+docker stop nextjs-docker
 
 echo "2. remove container"
-sudo docker rm nextjs-docker
+docker rm nextjs-docker
 
 echo "3. start container"
-sudo docker run -p 3000:3000 -d nextjs-docker
+docker run -p 3000:3000 -d nextjs-docker
 
-sudo docker ps
+docker ps
